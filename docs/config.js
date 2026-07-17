14c9bdfe78ed6324a918356f648da2558e340dcb
abanoub-maged145 <xmanabanoub@gmail.com>
2026-07-17 20:50:39 +0300

    تغيير كلمة السر

--- docs/config.js
+++ docs/config.js
@@ -1,2 +1,2 @@
 // كلمة السر (مخزنة كبصمة SHA-256) — للتغيير استخدم سكريبت change-password.ps1
-window.PASS_HASH = '158a323a7ba44870f23d96f1516dd70aa48e9a72db4ebb026b0a89e212a208ab';
+window.PASS_HASH = '7e1bfbe786c2c54ed123721ab83ca670d390024891ffe3690c93007b2705794d';
